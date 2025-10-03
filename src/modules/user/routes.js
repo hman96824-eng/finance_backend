814ec758981e6170458d319be9d6655c5dcc6c67
MZeeCoder <talentedparnter2022@gmail.com>
2025-10-03 17:54:58 +0500

    zee 6

--- src/modules/user/routes.js
+++ src/modules/user/routes.js
@@ -19,72 +19,26 @@ router
   // .get("/google", passport.authenticate("google", { scope: ["profile", "email"] }))
   .put("/upload-avatar", middleware.authenticate, upload.single("avatar"), service.uploadProfileImage)
   .delete("/remove-avatar", middleware.authenticate, service.removeProfileImage)
-  .get(
-    "/inactive",
-    middleware.authenticate,
-    checkPermission(["manage_users"]),
-    userController.InactiveUserStatus
-  )
+  .get("/inactive", middleware.authenticate, checkPermission(["manage_users"]), userController.InactiveUserStatus)
   // asim
   .post("/login", validate(validation.loginValidation), userController.login)
   .post("/refresh-token", userController.refreshToken)
 
-  .post(
-    "/forgetPasswordOtp",
-    validate(validation.requestOTP),
-    userController.forgetpassword
-  )
-  .post(
-    "/ForgetVerifyOtp",
-    validate(validation.verifyOTP),
-    userController.verifyCode
-  )
-  .post(
-    "/forgetPassword",
-    validate(validation.resetPassword),
-    userController.resetPassword
-  )
-  .post(
-    "/changePasswordOtp",
-    middleware.authenticate,
-    validate(validation.requestOTP),
-    userController.requestOtp
-  )
-  .post(
-    "/chnageVerifyOtp",
-    middleware.authenticate,
-    validate(validation.verifyOTP),
-    userController.verifyOtp
-  )
-  .post(
-    "/changepassword",
-    middleware.authenticate,
-    validate(validation.resetPassword),
-    userController.changePassword
-  )
+  .post("/forgetPasswordOtp", validate(validation.requestOTP), userController.forgetpassword)
+  .post("/ForgetVerifyOtp", validate(validation.verifyOTP), userController.verifyCode)
+  .post("/forgetPassword", validate(validation.resetPassword), userController.resetPassword)
+  .post("/changePasswordOtp", middleware.authenticate, validate(validation.requestOTP), userController.requestOtp)
+  .post("/chnageVerifyOtp", middleware.authenticate, validate(validation.verifyOTP), userController.verifyOtp)
+  .post("/changepassword", middleware.authenticate, validate(validation.resetPassword), userController.changePassword)
   // Profile
   // get only can admin and manager
-  .get(
-    "/",
-    middleware.authenticate,
-    checkPermission(["view_users"]),
-    userController.getUser
-  )
-  .get(
-    "/:id",
-    middleware.authenticate,
-    checkPermission(["view_users"]),
-    userController.getUserById
-  )
+  .get("/", middleware.authenticate, checkPermission(["view_users"]), userController.getUser)
+  .get("/:id", middleware.authenticate, checkPermission(["view_users"]),
+    userController.getUserById)
   .get("/health", userController.health)
 
   // User's Status
-  .delete(
-    "/remove/:id",
-    middleware.authenticate,
-    checkPermission(["view_users"]),
-    userController.RemoveUnacceptedUser
-  )
+  .delete("/remove/:id", middleware.authenticate, checkPermission(["view_users"]), userController.RemoveUnacceptedUser)
   // Send Invitation
   .post("/invite", middleware.authenticate, checkPermission(["view_users"]), validate(validation.inviteUserValidation), userController.sendInvitation)
   .post("/register", validate(validation.completeRegistrationValidation), userController.completeRegistration)
