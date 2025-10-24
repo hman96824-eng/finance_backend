1dfe7388c2d2f8644b572b4df60e3eb058bd6576
Asim-coder353 <d13734097@gmail.com>
2025-10-24 20:20:19 +0500

    refactor: streamline user and role routes for improved clarity and structure

--- src/modules/role/routes.js
+++ src/modules/role/routes.js
@@ -7,19 +7,15 @@ import roleController from "./controller.js";
 
 const router = express.Router();
 router.use(middleware.authenticate);
-
-const viewRouter = express.Router();
-viewRouter
-  .get("/", roleController.getAllRoles)
-  .get("/:id", roleController.getRoleById);
-router.use("/view", checkPermission(["view_users"]), viewRouter);
-
-const manageRouter = express.Router();
-console.log("Setting up role management routes");
-manageRouter
+router.get("/", roleController.getAllRoles); // get all roles
+router
+  .use(checkPermission(["view_users"]))
+  .get("/:id", roleController.getRoleById); // get role by id
+router
+  .use(checkPermission(["assign_roles"]))
   .post("/add", validate(validation.addRoleValidation), roleController.addRole) // add role
   .put("/update/:id", roleController.updateRole) // update role permissions
   .delete("/delete/:id", roleController.deleteRole) // delete role
   .put("/update-roleinfo/:id", roleController.updateRoleInfo); // update role info
-router.use("/manage", checkPermission(["assign_roles"]), manageRouter);
+
 export default router;
