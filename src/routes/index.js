4097385b7b8728e75108e32bf351aa8b0157ba48
Asim-coder353 <d13734097@gmail.com>
2025-11-07 17:51:06 +0500

    Fix formatting inconsistencies in route imports for improved code clarity

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import authUser from "../modules/user/routes.js";
-import roleRoutes from "../modules/role/routes.js"
-import inviteRoutes from "../modules/invites/routes.js"
-import orgRoutes from '../modules/organization/routes.js'
-import empRoutes from '../modules/employee/routes.js'
-import proRoutes from '../modules/project/routes.js'
+import roleRoutes from "../modules/role/routes.js";
+import inviteRoutes from "../modules/invites/routes.js";
+import orgRoutes from "../modules/organization/routes.js";
+import empRoutes from "../modules/employee/routes.js";
+import proRoutes from "../modules/project/routes.js";
 import fileRoutes from "../modules/fileRecord/routes.js";
 import express from "express";
 
@@ -14,10 +14,10 @@ export default (app) => {
   const apiRoute = express.Router();
   apiRoute.use("/users", authUser);
   apiRoute.use("/roles", roleRoutes);
-  apiRoute.use("/", inviteRoutes)
-  apiRoute.use('/organizations', orgRoutes)
-  apiRoute.use('/employees', empRoutes)
-  apiRoute.use('/projects', proRoutes)
+  apiRoute.use("/", inviteRoutes);
+  apiRoute.use("/organizations", orgRoutes);
+  apiRoute.use("/employees", empRoutes);
+  apiRoute.use("/projects", proRoutes);
   apiRoute.use("/files", fileRoutes);
 
   app.use("/api/v1", apiRoute);
