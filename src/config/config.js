1b938716a71bad0da90367d61a6f5a21fcc3b664
MZeeCoder <talentedparnter2022@gmail.com>
2025-11-15 13:30:38 +0500

    update CORS settings to allow all origins

--- src/config/config.js
+++ src/config/config.js
@@ -29,7 +29,7 @@ export const config = {
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
   corsSettings: {
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: '*',
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
