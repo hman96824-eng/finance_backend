a3c181014e484f2723907f968aca17796942fb9e
MZeeCoder <talentedparnter2022@gmail.com>
2025-11-15 13:50:49 +0500

    update: allow all origins in CORS configuration

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const port = config.PORT || 5000;
 const app = express();
 
 // ✅ CORS must be applied BEFORE routes
-app.use(cors(config.corsSettings));
+app.use(cors('*'));
 
 // app.use(express.json());
 app.use(express.json({ limit: "10mb" })); // default is 100kb
