730b791bcc537a3b0db58d7f7acd9a9d4e3dbbf9
MZeeCoder <talentedparnter2022@gmail.com>
2025-11-18 15:25:47 +0500

    update the server cors issue

--- src/server.js
+++ src/server.js
@@ -13,8 +13,9 @@ const port = config.PORT || 5000;
 
 const app = express();
 
-console.log("CORS ORIGIN:", config.CORS_ORIGIN);
-console.log("CORS SETTINGS:", config.corsSettings);
+console.log("CORS ORIGIN: ----", config.CORS_ORIGIN);
+
+console.log("CORS SETTINGS: ---", config.corsSettings);
 
 app.use(cors(config.corsSettings));
 
